8805c5ea1db6dac0ff08af2968a4e7cef8082111
jeck16 <khushiraval90@gmail.com>
2023-07-19 02:49:44 +0100

    week15

---
+++ src/test/java/resources/testdata/TestData.java
@@ -0,0 +1,14 @@
+package resources.testdata;
+
+
+public class TestData {
+    public Object[][] getData() {
+        Object[][] data = new Object[][]{
+                {"test1@gmail.com", "test123"},
+                {"test2@gmail.com", "test234"},
+                {"test3@gmail.com", "test345"}
+        };
+        return data;
+    }
+
+}
